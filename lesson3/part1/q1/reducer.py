59ba9b911d2dbafe8ee698ecd9d0b5149ba20acc
sid2305 <siddhant.lahoti2014@vit.ac.in>
2016-07-29 23:56:23 +0530

    Create reducer.py

---
+++ lesson3/part1/q1/reducer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+
+
+import sys
+
+salesTotal = 0
+oldKey = None
+
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+    if len(data) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data
+
+    if oldKey and oldKey != thisKey:
+        print oldKey, "\t", salesTotal
+        oldKey = thisKey;
+        salesTotal = 0
+
+    oldKey = thisKey
+    salesTotal += float(thisSale)
+
+if oldKey != None:
+    print oldKey, "\t", salesTotal
